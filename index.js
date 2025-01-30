21e8f5bf6b7b6eacfb145f768f32aac5d6bb2c65
asmohr714 <152821567+asmohr714@users.noreply.github.com>
2025-01-30 12:50:42 -0500

    cors

--- index.js
+++ index.js
@@ -10,28 +10,35 @@ const { check, validationResult } = require('express-validator');
 const Movies = Models.Movie;
 const Users = Models.User;
 
-const cors = require('cors');
-let allowedOrigins = ['https://my-flix-client-seven.vercel.app', 'http://localhost:1234', 'http://localhost:3000', 'http://localhost:8080', 'https://git.heroku.com/my-cinema-selector.git', 'https://asmohr-myflix-db.netlify.app/'];
+//const cors = require('cors');
+//let allowedOrigins = ['https://my-flix-client-seven.vercel.app', 'http://localhost:1234', 'http://localhost:3000', 'http://localhost:8080', 'https://git.heroku.com/my-cinema-selector.git', 'https://asmohr-myflix-db.netlify.app/'];
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message), false);
-    }
-    return callback(null, true);
-  }
-}));
+//app.use(cors({
+//  origin: (origin, callback) => {
+//    if (!origin) return callback(null, true);
+//    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+//      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+//      return callback(new Error(message), false);
+//   }
+//    return callback(null, true);
+ // }
+//}));
 
-// app.use(cors({ origin: '*', methods: 'GET,POST,PUT,DELETE', allowedHeaders: 'Content-Type,Authorization', optionsSuccessStatus: 204 }));
-
+// Disable CORS
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000, https://my-flix-client-seven.vercel.app'); // Allow requests from your React app
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Specify allowed methods
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Specify allowed headers
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
+ app.use(cors({ origin: '*', methods: 'GET,POST,PUT,DELETE', allowedHeaders: 'Content-Type,Authorization', optionsSuccessStatus: 204 }));
+
+//app.use((req, res, next) => {
+//  res.header('Access-Control-Allow-Origin', 'http://localhost:3000, https://my-flix-client-seven.vercel.app'); // Allow requests from your React app
+//  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Specify allowed methods
+//  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Specify allowed headers
+//  next();
+//});
 
 
 // Middleware
