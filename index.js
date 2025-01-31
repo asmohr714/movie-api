378bcf44a6d10622fe1d412c44b15e96e40acea0
asmohr714 <152821567+asmohr714@users.noreply.github.com>
2025-01-31 17:00:56 -0500

    cors

--- index.js
+++ index.js
@@ -28,20 +28,20 @@ const allowedOrigin = [
  'https://my-flix-client-seven.vercel.app/login'
 ];
 
-//app.use(
-//	cors({
-//		origin: (origin, callback) => {
-//			if (!origin || allowedOrigin.includes(origin)) {
-//				callback(null, true);
-//			} else {
-//				callback(new Error(`CORS policy does not allow access from origin: ${origin}`));
-//			}
-//		},
-//		methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//		allowedHeaders: ['Content-Type', 'Authorization'],
-//		credentials: true,
-//	})
-//);
+app.use(
+	cors({
+		origin: (origin, callback) => {
+			if (!origin || allowedOrigin.includes(origin)) {
+				callback(null, true);
+			} else {
+				callback(new Error(`CORS policy does not allow access from origin: ${origin}`));
+			}
+		},
+		methods: ['GET', 'POST', 'PUT', 'DELETE'],
+		allowedHeaders: ['Content-Type', 'Authorization'],
+		credentials: true,
+	})
+);
 
 // Disable CORS
 
