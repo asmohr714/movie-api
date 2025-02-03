1de4d245df825515ad46649c89fabf4e54eb93b2
asmohr714 <152821567+asmohr714@users.noreply.github.com>
2025-02-02 20:58:32 -0500

    cors

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.use(
 			}
 		},
 		methods: ['GET', 'POST', 'PUT', 'DELETE'],
-		allowedHeaders: ['Content-Type', 'Authorization'],
+		allowedHeaders: ['Content-Type', 'Authorization', 'access-control-allow-origin'],
 		credentials: true,
 	})
 );
